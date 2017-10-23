de71c609fdbf40604479f52eb2c939f843cde0a6
tianchen <tczhong24@gmail.com>
2017-10-22 22:40:00 -0400

    clean

--- choosestock.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from hurst import hurst_rs
-import pandas as pd
-import os
-for file in os.walk('aaa'):
-    for ff in file[2]:
-        df = pd.read_csv("aaa/"+ff)
-        print(hurst_rs(df['close'].values))
